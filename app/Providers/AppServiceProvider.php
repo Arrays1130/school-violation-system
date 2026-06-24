3f909816783f9d2d085d6fb5a308534588951125
Inui30 <castillanesjaytzy@gmail.com>
2026-06-24 23:16:56 +0800

    Force HTTPS scheme on production for assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 
     /**
